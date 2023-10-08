1b8c5f2505cac590b2ce4faa6d6f2d5476280914
PramAhmad <pramuditaadit2810@gmail.com>
2023-10-09 00:54:15 +0700

    update activity

--- pages/admin/activity/[id]/index.vue
+++ pages/admin/activity/[id]/index.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <NuxtLayout name="authenticated">
+            <!-- loading timpa all elemet -->
+            <div v-if="loading" class="flex justify-center items-center h-screen">
+                <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+            </div>
                 <!-- single layout -->
+
                 <CardBox >
     <BaseLevel type="justify-around lg:justify-center h-full">
       <img :src="fotopath" 
@@ -20,6 +25,43 @@
       </div>
     </BaseLevel>
   </CardBox>
+  <form method="post" @submit.prevent="updateActivity">
+        <CardBox>
+          <FormField label="New Profile" help="Max 500kb">
+            <FotoActivity model:path="fotopath"/>
+          </FormField>
+
+          <FormField label="title" help="Masukan Title baru">
+            <FormControl
+            v-model="title"
+              :icon="mdiAccount"
+              name="nama"
+              required
+              autocomplete="nama"
+            />
+          </FormField>
+          <FormField label="desc" help="Masukan desc baru">
+            <FormControl
+              v-model="desc"
+              :icon="mdiMail"
+              type="text"
+              name="desc"
+              required
+              autocomplete="desc"
+            />
+          </FormField>
+       
+
+       
+            <BaseButtons>
+              <button type="submit" class="py-2 px-5 bg-sky-600 rounded-md text-white hover:bg-sky-500">Update</button>
+
+              <BaseButton color="info" label="Options" outline />
+            </BaseButtons>
+         
+          </CardBox>
+        </form>
+
         </NuxtLayout>
     </div>
 </template>
@@ -31,6 +73,7 @@ const $route = useRoute()
 const fotopath = ref('')
 const desc = ref('')
 const title = ref('')
+const loading = ref(false)
 const getSingleActivity = async()=>{
     const {data,error} = await supabase.from("activity").select().eq('id',$route.params.id)
    fotopath.value = data[0].image
@@ -40,6 +83,30 @@ const getSingleActivity = async()=>{
         console.log(error)
     }
 }
+const updateActivity = async ()=>{
+    loading.value = true
+    // validate jika tidak ada foto
+    if(fotopath.value){
+
+        const {data,error} = await supabase.from("activity").update({image:fotopath.value,title:title.value,desc:desc.value}).eq('id',$route.params.id)
+        if(error){
+            console.log(error)
+        }
+        else{
+        }
+        loading.value = false
+    }
+    else{
+        const {data,error} = await supabase.from("activity").update({title:title.value,desc:desc.value}).eq('id',$route.params.id)
+        if(error){
+            console.log(error)
+        }
+        else{
+        }
+        loading.value = false
+    }
+}
+
 onMounted(()=>{
     getSingleActivity()
 })
