4fa3f0dbc9a54b7152d974684657a72b9d24fa1c
PramAhmad <pramuditaadit2810@gmail.com>
2023-11-19 15:36:23 +0700

    coment fitur

--- .nuxt/types/imports.d.ts
+++ .nuxt/types/imports.d.ts
@@ -125,6 +125,7 @@ declare global {
   const useModel: typeof import('vue')['useModel']
   const useNuxtApp: typeof import('../../node_modules/nuxt/dist/app')['useNuxtApp']
   const useNuxtData: typeof import('../../node_modules/nuxt/dist/app')['useNuxtData']
+  const useNuxtDevTools: typeof import('../../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools')['useNuxtDevTools']
   const usePinia: typeof import('../../node_modules/@pinia/nuxt/dist/runtime/composables')['usePinia']
   const useRequestEvent: typeof import('../../node_modules/nuxt/dist/app')['useRequestEvent']
   const useRequestFetch: typeof import('../../node_modules/nuxt/dist/app')['useRequestFetch']
@@ -288,6 +289,7 @@ declare module 'vue' {
     readonly useModel: UnwrapRef<typeof import('vue')['useModel']>
     readonly useNuxtApp: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useNuxtApp']>
     readonly useNuxtData: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useNuxtData']>
+    readonly useNuxtDevTools: UnwrapRef<typeof import('../../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools')['useNuxtDevTools']>
     readonly usePinia: UnwrapRef<typeof import('../../node_modules/@pinia/nuxt/dist/runtime/composables')['usePinia']>
     readonly useRequestEvent: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useRequestEvent']>
     readonly useRequestFetch: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useRequestFetch']>
@@ -445,6 +447,7 @@ declare module '@vue/runtime-core' {
     readonly useModel: UnwrapRef<typeof import('vue')['useModel']>
     readonly useNuxtApp: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useNuxtApp']>
     readonly useNuxtData: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useNuxtData']>
+    readonly useNuxtDevTools: UnwrapRef<typeof import('../../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools')['useNuxtDevTools']>
     readonly usePinia: UnwrapRef<typeof import('../../node_modules/@pinia/nuxt/dist/runtime/composables')['usePinia']>
     readonly useRequestEvent: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useRequestEvent']>
     readonly useRequestFetch: UnwrapRef<typeof import('../../node_modules/nuxt/dist/app')['useRequestFetch']>
--- pages/sifest/index.vue
+++ pages/sifest/index.vue
@@ -19,8 +19,8 @@
     <div class="mt-16">
     
 
-          <div class="flex flex-wrap justify-between  w-full  gap-y-10 mb-10" >
-    <div class="md:max-w-[45%] w-full bg-white border-4 border-slate-100 rounded-sm p-5 " v-for="data in datas" :key="data.id">
+          <div class="flex flex-wrap justify-start  w-full  gap-y-10 mb-10" >
+    <div class="md:max-w-[33%] w-full bg-white border-4 border-slate-100 rounded-sm p-5 " v-for="data in datas" :key="data.id">
       <div class="flex justify-end">
         <div class="bg-[#63B4FF] text-white rounded-md px-2 py-1" >
           <p class="text-sm">From <span class="font-bold">{{ data.from }}</span></p>
@@ -35,6 +35,13 @@
       <div class="w-full py-5 rounded-md bg-slate-100  mt-3 p-3">
           <p class="text-slate-500 text-md">{{ data.msg }}</p>
       </div>
+      <!-- komentar button -->
+      <button @click="getKomentar(data.id)" class="py-1 px-1 rounded-md text-white bg-[#63B4FF]  mt-3">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+</svg>
+
+      </button>
     </div>
  
         </div>
@@ -44,6 +51,68 @@
 
 
     </div>
+    <div :class="{ 'block': showModal, 'hidden': !showModal }" class="fixed z-10 inset-0 overflow-y-auto w-full">
+    <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <!-- Background overlay -->
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-black opacity-75"></div>
+      </div>
+      
+      <!-- Modal content -->
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+      <div :class="{ 'inline-block': showModal, 'hidden': !showModal }" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:my-8 align-middle sm:w-full sm:max-w-lg w-full">
+        <!-- Modal content here -->
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <!-- Modal header -->
+          <div class="sm:flex sm:items-start">
+  <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+    <div class="flex justify-end">
+      <button @click="closeModal()" type="button" class="focus:outline-none">
+        <svg
+          class="w-6 h-6 text-gray-500 hover:text-gray-700"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+    </div>
+    <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">Komentar</h3>
+   
+              
+              <p v-if="loading">loading</p>
+              <div class="mt-2" v-for="c in coment" :key="c.id">
+                <!-- Modal body content -->
+               <!-- loop komentar -->
+               <div class="w-full bg-slate-100 border-2 border-slate-200 text-gray-700 rounded-md py-2"> 
+                <p class="text-grary-700 text-left px-3">{{ c.msg }}</p>
+               </div>
+              </div>
+              <div class="mt-5">
+                <hr class="mb-5">
+             
+                <form @submit.prevent="addKomentar()" method="post">
+                <div class="flex">
+                  <input type="text" class="w-full border-2 border-slate-200 rounded-md px-2 py-1 focus:outline-none focus:border-[#63B4FF]" placeholder="masukan komentar" v-model="komentar" />
+                  <button type="submit" class="bg-[#63B4FF] text-white rounded-md px-2 py-1 ml-2">Kirim</button>
+                </div>
+              </form>
+              </div>
+            </div>
+          </div>
+        </div>
+  
+       
+      </div>
+    </div>
+  </div>
     <!-- <div class="" style="width: 100%; height: 100%">
       
         <div v-for="(icon, index) in icons" :key="index" :style="{ top: `${icon.top}px`, left: `${icon.left}px` }" class="absolute ">
@@ -64,9 +133,18 @@
     </div>
 </template>
 <script setup>
+import ko from 'date-fns/esm/locale/ko';
+
 const supabase = useSupabaseClient()
 const datas = ref([])
+const coment = ref([])
+const komentar = ref('')
+const id_sifest = ref(0)
+const loading = ref(false)
+let showModal = ref(false);
+
 const getSifest = async ()=>{
+
     const {data,error} = await supabase.from('sifest').select()
     .order('id', { ascending: false })
     .eq('is_accept',1)
@@ -77,6 +155,47 @@ const getSifest = async ()=>{
         datas.value = data
     }
 }
+
+const getKomentar = async (id)=>{
+    showModal.value = !showModal.value;
+    loading.value = true
+    id_sifest.value = id
+    const {data,error} = await supabase
+    .from("komentar")
+    .select()
+    .eq("id_sifest",id)
+    if (error){
+        console.log(error)
+    }
+    else{
+        coment.value = data
+        loading.value = false
+    }
+}
+
+const closeModal = ()=>{
+    showModal.value = !showModal.value;
+    // kosongkan coment
+    coment.value = []
+}
+const addKomentar = async ()=>{
+    const {data,error} = await supabase
+    .from("komentar")
+    .insert([
+        {id_sifest:id_sifest.value,msg:komentar.value}
+    ])
+    if (error){
+        console.log(error)
+    }
+    else{
+    
+        coment.value = []
+        komentar.value = ''
+        getKomentar(id_sifest.value)
+    }
+}
+
+
 
 onMounted(()=>{
     getSifest()
